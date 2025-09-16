506ba58ddf0be50d4556340e9f826ab45ea0f533
codythompson <cody.thompson22@gmail.com>
2025-09-16 13:11:23 -0700

    prettified lint system ent

--- src/entities/system.ts
+++ src/entities/system.ts
@@ -2,26 +2,32 @@ import { Entity, EntityPushParams } from "../entities";
 import { TemplateParams } from "../template";
 import { PositionTypes } from "./components/position";
 
-export interface System extends Entity<"system", PositionTypes> {};
+export type System = Entity<"system", PositionTypes>;
 
 export interface SystemTemplateParams extends TemplateParams<System["type"]> {
-  x: number
-  y: number
-  w: number
-  h: number
-};
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
 
-export function makeSystem({name,x,y,w,h}:SystemTemplateParams):EntityPushParams<System> {
+export function makeSystem({
+  name,
+  x,
+  y,
+  w,
+  h,
+}: SystemTemplateParams): EntityPushParams<System> {
   return {
     entity: {
       type: "system",
       name,
       components: {
         position: { type: "position", x, y },
-        dimension: { type: "dimension", w, h }
-      }
+        dimension: { type: "dimension", w, h },
+      },
     },
-    sprites: {}
+    sprites: {},
     // children
-  }
+  };
 }
